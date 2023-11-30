64fc7ce0cdd2dacb4216fa119c7a75eacd53fec1
trungduc1294 <trungduc.1294@gmail.com>
2023-11-30 21:56:08 +0700

    commit code

---
+++ src/components/PanelInviteMember.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import type { Project, User } from "../lib/types";
+  import OverlayPanel from "./OverlayPanel.svelte";
+  export let visible: boolean;
+  export let project: Project;
+  let email = "";
+  let membersToBeAdded: User[] = [];
+  let foundMembers: User[] = []; // find when email change;
+
+  function hide() {
+    visible = false;
+  }
+  function submit() {
+    // invite members to projects
+  }
+  function addMember(member: User) {
+    membersToBeAdded.push(member);
+  }
+</script>
+
+<OverlayPanel bind:visible>
+  <form on:submit|preventDefault={submit}>
+    <div>
+      {#each membersToBeAdded as member}
+        <div>
+          <p>{member.name}</p>
+        </div>
+      {/each}
+    </div>
+    <input type="text" name="" id="" placeholder="Email" bind:value={email} />
+    <div>
+      {#each foundMembers as foundMember}
+        <button on:click={() => addMember(foundMember)}
+          >{foundMember.name}</button
+        >
+      {/each}
+    </div>
+
+    <button on:click={hide}>Cancel</button>
+    <button type="submit">Invite</button>
+  </form>
+</OverlayPanel>
