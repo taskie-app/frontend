209f0936b72ef19feff43d57efb9f1df5caf8022
Nguyen Hai Duc <nguyenhaiduc06@gmail.com>
2023-11-30 14:22:15 +0700

    UPDATE: api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,6 @@
 import axios, { Axios } from "axios";
-import type { ApiResult } from "./types";
+import type { ApiResult, Project } from "./types";
+import { API_BASE_URL } from "./config";
 
 class Api {
   private axios: Axios;
@@ -26,29 +27,38 @@ class Api {
     return { data: { token }, error };
   }
 
-  async getAuthenticated() {
-    return { error: true };
+  async getAuthenticated(): ApiResult<{ authenticated: boolean }> {
+    return { data: { authenticated: true }, error: null };
   }
 
-  async getProjects() {
+  async getProjects(): ApiResult<{ projects: Project[] }> {
     const { data } = await this.axios.get("/projects/");
-    return { data: data.data, error: data.error };
+    const { projects, error } = data;
+    return { data: { projects }, error };
   }
 
-  async createProject(name: string, description: string) {
+  async createProject(
+    name: string,
+    description: string
+  ): ApiResult<{ project: Project }> {
     const { data } = await this.axios.post("/projects/", {
       name,
       description,
-      manager: {},
-      members: [],
     });
-    return { data: data.data, error: data.error };
+    const { project, error } = data;
+    return { data: { project }, error };
   }
 
   async getProjectDetails(id: string) {
     const { data } = await this.axios.get(`/projects/${id}`);
     return { data: data.data, error: data.error };
   }
+
+  async getProjectTasks(id: string) {
+    const { data } = await this.axios.get(`/tasks?projectId=${id}`);
+    const { tasks, error } = data;
+    return { data: { tasks }, error };
+  }
 }
 
-export const api = new Api("//localhost:4000");
+export const api = new Api(API_BASE_URL);
