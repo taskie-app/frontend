440d38929cb4880735719fddac3ecc3397d90a26
Nguyen Hai Duc <nguyenhaiduc06@gmail.com>
2023-11-30 21:00:48 +0700

    ADD: overlay panel component

---
+++ src/components/OverlayPanel.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { clickOutside } from "../actions/clickOutside";
+  export let visible: boolean;
+  function hide() {
+    visible = false;
+  }
+</script>
+
+<div
+  class={`absolute top-0 left-0 w-screen h-screen ${
+    visible ? "flex" : "hidden"
+  } bg-black/50 items-center justify-center`}
+>
+  <div
+    class="bg-white w-full max-w-2xl"
+    use:clickOutside
+    on:click_outside={hide}
+  >
+    <slot />
+  </div>
+</div>
