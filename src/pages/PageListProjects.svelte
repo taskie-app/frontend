1112e1de274f811df6dc4e9d02e7981136052332
Nguyen Hai Duc <nguyenhaiduc06@gmail.com>
2023-12-01 16:13:30 +0700

    UPDATE: projects ui

--- src/pages/PageListProjects.svelte
+++ src/pages/PageListProjects.svelte
@@ -1,33 +1,26 @@
 <script>
   import ProjectItem from "../components/ProjectItem.svelte";
+  import { fetchProjects, projects } from "../stores/projectStore";
+  import PanelCreateProject from "../components/PanelCreateProject.svelte";
   import { onMount } from "svelte";
-  import { authenticated } from "../stores/auth";
-  import { replace } from "svelte-spa-router";
-  import { api } from "../lib/api";
-  import { projects } from "../stores/projectStore";
-  let modalVisible = false;
-  let name = "";
-  let description = "";
+  let createProjectPanelVisible = false;
 
-  function showModal() {
-    modalVisible = true;
-  }
-
-  function hideModal() {
-    modalVisible = false;
-  }
-
-  function submitCreate() {
-    api.createProject(name, description);
-  }
+  onMount(() => {
+    fetchProjects();
+  });
 </script>
 
 <div class="flex flex-1 flex-col">
-  <div class="h-16 border-b px-8 flex items-center justify-between">
-    <h1 class="text-lg font-medium">Projects</h1>
-    <button on:click={showModal}>New Project</button>
-  </div>
   <div class="flex-1 p-8 space-y-4">
+    <button
+      class="border"
+      on:click={() => {
+        createProjectPanelVisible = true;
+      }}>New project</button
+    >
+
+    <h1>Projects</h1>
+
     <ul
       class="mx-auto grid grid-cols-1 gap-4 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3"
     >
@@ -40,27 +33,4 @@
   </div>
 </div>
 
-<div
-  class={`absolute top-0 left-0 w-screen h-screen ${
-    modalVisible ? "flex" : "hidden"
-  } bg-black/50 items-center justify-center`}
->
-  <div class="bg-white w-full max-w-2xl">
-    <input
-      type="text"
-      name=""
-      id=""
-      placeholder="Project name"
-      bind:value={name}
-    />
-    <input
-      type="text"
-      name=""
-      id=""
-      placeholder="Description"
-      bind:value={description}
-    />
-    <button on:click={hideModal}>Cancel</button>
-    <button on:click={submitCreate}>Create</button>
-  </div>
-</div>
+<PanelCreateProject bind:visible={createProjectPanelVisible} />
--- src/pages/PageProjectDetails.svelte
+++ src/pages/PageProjectDetails.svelte
@@ -1,22 +1,59 @@
 <script lang="ts">
   import MemberItem from "../components/MemberItem.svelte";
   import PanelInviteMember from "../components/PanelInviteMember.svelte";
+  import PanelCreateTask from "../components/PanelCreateTask.svelte";
   import TaskBoard from "../components/TaskBoard.svelte";
   import { projects } from "../stores/projectStore";
+  import type { Task } from "../lib/types";
+  import { api } from "../lib/api";
+  import TaskList from "../components/TaskList.svelte";
 
   export let params: { id: string };
-  $: project = $projects.filter((project) => project._id == params.id)[0];
+
+  let tasks: Task[] = [];
+  let tasksDisplayMode: "BOARD" | "LIST" = "BOARD";
   let inviteMemberPanelVisible = false;
+  let createTaskPanelVisible = false;
+
+  // on params change, update the project
+  $: project = $projects.filter((project) => project._id == params.id)[0];
+  // on project change, fetchTasks
+  $: project && fetchTasks();
+
+  async function fetchTasks() {
+    const { tasks: fetchedTasks, error } = await api.getProjectTasks(
+      project._id
+    );
+    if (error) {
+      alert(error);
+    } else {
+      tasks = fetchedTasks;
+    }
+  }
+
+  async function changeTaskStatus(
+    taskId: Task["_id"],
+    newStatus: Task["status"]
+  ) {
+    // change the tasks state for instant feedback
+    tasks = tasks.map((task) =>
+      task._id == taskId ? { ...task, status: newStatus } : task
+    );
+
+    // update status to db
+    // if fail, undo the tasks state change
+  }
+
+  function changeTasksDisplayMode(newMode: "BOARD" | "LIST") {
+    tasksDisplayMode = newMode;
+  }
 </script>
 
 <div class="flex flex-col">
   <div class="flex-1 p-8 space-y-4">
     <div>
-      <p class="text-gray-400">
-        Projects / <span class="text-gray-800">{project?.name}</span>
-      </p>
       <h1 class="text-3xl font-medium">{project?.name}</h1>
-      <h1 class="text-xl">{project?.description}</h1>
+      <h1 class="text-xl text-gray-400">{project?.description}</h1>
     </div>
 
     <div class="flex items-center gap-2">
@@ -32,14 +69,19 @@
         Invite</button
       >
       <div class="flex-1"></div>
-      <button>List</button>
-      <button>Board</button>
+      <button on:click={() => changeTasksDisplayMode("LIST")}>List</button>
+      <button on:click={() => changeTasksDisplayMode("BOARD")}>Board</button>
       <button>Settings</button>
-      <button>New task</button>
+      <button on:click={() => (createTaskPanelVisible = true)}>New task</button>
     </div>
 
-    <TaskBoard />
+    {#if tasksDisplayMode == "BOARD"}
+      <TaskBoard {project} {tasks} onTaskDropped={changeTaskStatus} />
+    {:else}
+      <TaskList {project} {tasks} />
+    {/if}
   </div>
 </div>
 
 <PanelInviteMember bind:visible={inviteMemberPanelVisible} {project} />
+<PanelCreateTask bind:visible={createTaskPanelVisible} {project} />
