19fa34d7b3a69e20cac96b65750823e6d8219296
trungduc1294 <trungduc.1294@gmail.com>
2023-12-03 10:49:42 +0700

    UPDATE: components

---
+++ src/components/TaskListItem.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { link } from "svelte-spa-router";
+  import type { Project, Task } from "../lib/types";
+
+  export let project: Project;
+  export let task: Task;
+</script>
+
+<a href={`/tasks/${project._id}/${task._id}`} class="contents" use:link>
+  <div class="whitespace-nowrap py-2 text-gray-700">{task.name}</div>
+  <div class="whitespace-nowrap py-2 text-gray-700">
+    {task.description}
+  </div>
+  <div class="whitespace-nowrap py-2 text-gray-700">{task.status}</div>
+  <div class="whitespace-nowrap py-2 text-gray-700">{task.dueDate}</div>
+  <div class="whitespace-nowrap py-2 text-gray-700">
+    {task.assignedTo ?? "Unassigned"}
+  </div>
+</a>
